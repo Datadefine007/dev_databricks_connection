4729de7d3791c05dc88500b1d982939fe4e00c97
AjaySingh Jadoun <ajaysingh.jadoun@celebaltech.com>
2023-12-14 14:18:49 +0530

    hello

--- vscode_x_databricks.py
+++ vscode_x_databricks.py
@@ -31,3 +31,30 @@ for i in dem_task:
     d2*=i
 print(d2)
     
+
+
+
+
+# Upcoming Dataframe
+from pyspark.sql import SparkSession
+from pyspark.sql import Row
+import random
+
+num_records = 20
+
+data = []
+classes = ["Class 1", "Class 2", "Class 3", "Class 4", "Class 5"]
+locations = ["Delhi", "Mumbai", "Chennai", "Kolkata", "Bangalore"]
+
+for _ in range(num_records):
+    name = f"Student{_ + 1}"
+    class_name = random.choice(classes)
+    score = round(random.uniform(50, 100), 2) 
+    attendance_rate = round(random.uniform(70, 100), 2)  
+    location = random.choice(locations)
+
+    data.append(Row(name=name, class=class_name, score=score, attendance_rate=attendance_rate, location=location))
+
+df = spark.createDataFrame(data)
+
+df.display()
